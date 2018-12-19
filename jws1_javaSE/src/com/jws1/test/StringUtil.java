07ba10731f8636e043a605aed5ffb61279a03dea
BAHARNOTEBOOK\user <baharkutlug@gmail.com>
2018-12-20 00:37:36 +0300

    StringUtil clasına açıklamalara göre fonksiyonlar eklendi.  - deleteTheSpaces  - reverse  - deleteFirstAndLastCharacter  - splitBySplitter

--- jws1_javaSE/src/com/jws1/test/StringUtil.java
+++ jws1_javaSE/src/com/jws1/test/StringUtil.java
@@ -1,5 +1,7 @@
 package com.jws1.test;
 
+import java.math.BigInteger;
+
 public class StringUtil {
 
 	/** 
@@ -19,51 +21,89 @@ public class StringUtil {
 	}
 	/** 
 	 *Author: Bahar
-	 *Tarih:
+	 *Tarih:2018/12/19
 	 *
 	 **/
 	
-	//Dışardan verilen parameter(String) başındaki ve sonundaki boşukları sil
+	//Dışardan verilen parameter(String) başındaki ve sonundaki boşlukları sil
+
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public String  deleteTheSpaces(String parameter)
+	{
+		//parameter = parameter.trim();
+		int last = parameter.length();
+		
+		StringBuffer sub = new StringBuffer();
+		
+		sub.append(parameter);
+		
+		sub.deleteCharAt(0);
+		sub.deleteCharAt(last);
+		
+		parameter.contentEquals(sub);
+		
+		return parameter;
+	}
+
 	
 	
 	
 	//Dışarıdan verilen parameter(String) bit(010101)'leri tersine çevir
 	
+	public String reverse (String parameter)
+	{
+		String binary = new BigInteger(parameter.getBytes()).toString();
+		
+		int last = binary.length();
+		
+		char[] sub=binary.toCharArray();
+		
+		int i;
+		for(i=0;i<=last;i++) {
+			
+			if(sub[i]==0) {
+				sub[i]=1;
+			}
+			else sub[i]=0;
+		}
+		
+		parameter = sub.toString();
+		
+		return parameter;
+	}
 	
 	
 	
 	
+	// Dışarıdanan girilen String değişkenin başındaki ve sonundaki karakteri sil
+	
+	public String deleteFirstAndLastCharacter(String parameter)
+	{
+		int last = parameter.length();
+		
+		StringBuffer sub = new StringBuffer();
+		
+		sub.append(parameter);
+		
+		sub.deleteCharAt(0);
+		sub.deleteCharAt(last);
+		
+		parameter.contentEquals(sub);
+		
+		return parameter;
+	}
 	
 	
 	
+	// ayraçlarla ayrılmış uzun bir string parçalarına aır
+	 
+	public String splitBySplitter (String parameter, String splitter)
+	{
+		parameter.split(splitter);
+		
+		return parameter;
+	}
 	
-	
-	
-	
-	
-	
-	// Dışarıdan girilen String değişkenin başındaki ve sonundaki karakteri sil
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 }
