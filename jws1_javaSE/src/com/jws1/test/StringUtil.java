b2f7dadd891ea0b8f076ba72dd9da6bf3dd7e12a
FIT\soner.sariusak <soner.sariusak@foriba.com>
2018-12-22 01:41:01 +0300

    -- string metodlar için main metodu yazıldı

--- jws1_javaSE/src/com/jws1/test/StringUtil.java
+++ jws1_javaSE/src/com/jws1/test/StringUtil.java
@@ -1,109 +1,81 @@
 package com.jws1.test;
 
-import java.math.BigInteger;
-
 public class StringUtil {
 
-	/** 
-	 *Author: Soner
-	 *2018.12.15
+	/**
+	 * Author: Soner 2018.12.15
 	 *
 	 **/
-	public boolean isNullorEmpty(String parameter)
-	{
-		
-		if(null==parameter || parameter.isEmpty())
-		{
+	public boolean isNullorEmpty(String parameter) {
+
+		if (null == parameter || parameter.isEmpty()) {
 			return false;
 		}
 		return true;
-		
+
 	}
-	/** 
-	 *Author: Bahar
-	 *Tarih:2018/12/19
+
+	/**
+	 * Author: Bahar Tarih:2018/12/19
 	 *
 	 **/
-	
-	//Dışardan verilen parameter(String) başındaki ve sonundaki boşlukları sil
 
-	
-	public String  deleteTheSpaces(String parameter)
-	{
-		//parameter = parameter.trim();
+	// Dışardan verilen parameter(String) başındaki ve sonundaki boşlukları sil
+
+	public String deleteTheSpaces(String parameter) {
+		// parameter = parameter.trim();
+
 		int last = parameter.length();
-		
-		StringBuffer sub = new StringBuffer();
-		
+
+		StringBuilder sub = new StringBuilder();
+
 		sub.append(parameter);
-		
+
 		sub.deleteCharAt(0);
-		sub.deleteCharAt(last);
-		
-		parameter.contentEquals(sub);
-		
-		return parameter;
+		sub.deleteCharAt(last - 2);
+
+		return sub.toString();
 	}
 
-	
-	
-	
-	//Dışarıdan verilen parameter(String) bit(010101)'leri tersine çevir
-	
-	public String reverse (String parameter)
-	{
-		String binary = new BigInteger(parameter.getBytes()).toString();
-		
-		int last = binary.length();
-		
-		char[] sub=binary.toCharArray();
-		
-		int i;
-		for(i=0;i<=last;i++) {
-			
-			if(sub[i]==0) {
-				sub[i]=1;
-			}
-			else sub[i]=0;
+	// Dışarıdan verilen parameter(String) bit(010101)'leri tersine çevir
+
+	public String reverse(String parameter) {
+
+		char[] sub = parameter.toCharArray();
+
+		for (int i = 0; i < parameter.length(); i++) {
+			if (sub[i] == '0') {
+				sub[i] = '1';
+			} else
+				sub[i] = '0';
 		}
-		
-		parameter = sub.toString();
-		
-		return parameter;
+		String value = new String(sub);
+		return value;
 	}
-	
-	
-	
-	
-	// Dışarıdanan girilen String değişkenin başındaki ve sonundaki karakteri sil
-	
-	public String deleteFirstAndLastCharacter(String parameter)
-	{
-		int last = parameter.length();
-		
-		StringBuffer sub = new StringBuffer();
-		
-		sub.append(parameter);
-		
-		sub.deleteCharAt(0);
-		sub.deleteCharAt(last);
-		
-		parameter.contentEquals(sub);
-		
-		return parameter;
-	}
-	
-	
-	
-	// ayraçlarla ayrılmış uzun bir string parçalarına aır
-	 
-	public String splitBySplitter (String parameter, String splitter)
-	{
-		parameter.split(splitter);
-		
-		return parameter;
-	}
-	
 
-	
+	// Dışarıdanan girilen String değişkenin başındaki ve sonundaki karakteri sil
+
+	public String deleteFirstAndLastCharacter(String parameter) {
+
+		int last = parameter.length();
+
+		StringBuilder sub = new StringBuilder();
+
+		sub.append(parameter);
+
+		sub.deleteCharAt(0);
+		sub.deleteCharAt(last - 2);
+
+		return sub.toString();
+	}
+
+	// ayraçlarla ayrılmış uzun bir string parçalarına aır
+
+	public String[] splitBySplitter(String parameter, String splitter) {
+
+		String[] value = parameter.split(splitter);
+
+		return value;
+	}
+
 }
